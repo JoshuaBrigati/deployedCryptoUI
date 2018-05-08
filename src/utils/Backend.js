58d538d3c89ccbc10907dc27949ffcaef0c86556
Grant Regimbal <regimbal.grant@gmail.com>
2018-05-07 19:51:19 -0500

    mucking with cors

--- src/utils/Backend.js
+++ src/utils/Backend.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 const Backend = axios.create({
   baseURL: process.env.REACT_APP_BACKEND_URL,
-  timeout: 1000
+  timeout: 1000,
+  headers: { 'Access-Control-Allow-Origin': '*' }
 });
 
 export default Backend;
