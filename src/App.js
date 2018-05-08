8f9acd8a6bc4c069ece35bf249994152b6d6ec03
Grant Regimbal <regimbal.grant@gmail.com>
2018-05-07 19:23:43 -0500

    show what env you are running

--- src/App.js
+++ src/App.js
@@ -3,6 +3,15 @@ import './App.css';
 import HeroUnit from './Components/HeroUnit';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  componentDidMount() {
+    console.log(process.env.NODE_ENV);
+  }
+
   render() {
     return (
       <div className="App">
--- src/Components/HeroUnit.js
+++ src/Components/HeroUnit.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class HeroUnit extends Component {
   render() {
-    return <h1> Welcome to Crypto Frontend</h1>;
+    return <h1> Welcome to Crypto Frontend {process.env.NODE_ENV}</h1>;
   }
 }
 
